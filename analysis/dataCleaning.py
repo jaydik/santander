3cdd0c65ce3fd9af7ce67b7bec04baa825ce21d0
Jon Dickerson <jaydik@gmail.com>
2016-04-26 21:17:04 -0400

    add data cleaning and presentation of variances.

--- analysis/dataCleaning.py
+++ analysis/dataCleaning.py
@@ -1,14 +1,34 @@
 import pandas as pd
 import numpy as np
-import xgboost as xgb
+from sklearn.feature_selection import SelectPercentile, f_classif
 
 
 def main():
 
     main_data = pd.read_csv('../data/train.csv')
 
-    train = xgb.DMatrix(main_data.as_matrix())
-    train.save_binary('../data/xgbtrain.buffer')
+    output = []
+    for x in main_data.columns:
+        output.append({
+            'variable': x,
+            'variance': main_data.ix[:, x].var(),
+            'corr_w_target': round(main_data.ix[:, x].corr(main_data.TARGET), 4),
+            'abs_corr': abs(round(main_data.ix[:, x].corr(main_data.TARGET), 4))}
+        )
+
+    # print csv for later in the presentation docs
+    variable_selector = pd.DataFrame(output)
+    variable_selector = variable_selector.set_index('variable')
+    variable_selector = variable_selector.drop('TARGET')
+    variable_selector.sort_values('abs_corr', ascending=False).to_csv('../presentationDocs/corrs.csv')
+
+    selector = SelectPercentile(f_classif, percentile=25)
+    test = selector.fit_transform(main_data.drop('TARGET', axis=1), main_data['TARGET'])
+
+    print(test.shape)
+
+
+
 
 if __name__ == '__main__':
     main()
